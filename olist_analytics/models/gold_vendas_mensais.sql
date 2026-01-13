2276f7bf7cd011658b10b8e093092b0c1ebb18b8
ricardoribs <ricardoribs12@gmail.com>
2026-01-13 16:53:15 -0300

    style(sql): lowercase keywords and fix lint errors

--- olist_analytics/models/gold_vendas_mensais.sql
+++ olist_analytics/models/gold_vendas_mensais.sql
@@ -1,16 +1,15 @@
 {{ config(materialized='table') }}
 
-WITH vendas_diarias AS (
-    SELECT
-        DATE_TRUNC('month', order_purchase_timestamp) AS mes_referencia
-        , COUNT(DISTINCT order_id) AS total_pedidos
-        , SUM(price) AS receita_total
-        , AVG(price) AS ticket_medio
-    -- CORREÇÃO: Usamos source() pois estamos lendo do Data Lake (Parquet), não de outro modelo dbt
-    FROM {{ source('lakehouse', 'orders_enriched') }}
-    WHERE order_status = 'delivered'
-    GROUP BY 1
+with vendas_diarias as (
+    select
+        date_trunc('month', order_purchase_timestamp) as mes_referencia
+        , count(distinct order_id) as total_pedidos
+        , sum(price) as receita_total
+        , avg(price) as ticket_medio
+    from {{ source('lakehouse', 'orders_enriched') }}
+    where order_status = 'delivered'
+    group by 1
 )
 
-SELECT * FROM vendas_diarias
-ORDER BY mes_referencia DESC
+select * from vendas_diarias
+order by mes_referencia desc
